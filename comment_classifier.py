f7da4168d7ad43cbd7dd85b9e1495512371301cd
vysakh-mykare <161435055+vysakh-mykare@users.noreply.github.com>
2024-02-28 11:39:30 +0530

    Update comment_classifier.py

--- comment_classifier.py
+++ comment_classifier.py
@@ -24,9 +24,9 @@ def classify_comment(comment):
 
 def to_excel(df):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
-    writer.save()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.save()
     processed_data = output.getvalue()
     return processed_data
 # Streamlit app
